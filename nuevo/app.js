4f5fd3a11099f49c4a95241bdad63d081064c9d9
Charly Bernabe <charly.ivan.bernabe@gmail.com>
2019-06-02 15:19:50 -0500

    Consultas

--- nuevo/app.js
+++ nuevo/app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var jwt = require('jsonwebtoken')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var bodyParser = require("body-parser");
+const pool = require('./database/db');
 var app = express();
+var router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,20 +27,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+
+
+
+//Autenticación de login
+app.post('/login', (req, res) => { 
+    var username = req.body.user 
+    var password = req.body.password
+
+    //Se hace la consulta a la bd
+    /*
+    HACE FALTA LA OCNSULTA AL SERVIDOR
+    */
+
+    if ((!(username === 'oscar' && password === '1234'))) {   
+        res.status(401).send({     
+            error: 'usuario o contraseña inválidos'   
+        }) 
+        return  
+    } 
+    var tokenData = {   
+        username: username    // Los datos de el usuario
+             
+    }
+    var token = jwt.sign(tokenData, 'SECRET', {    
+        expiresIn: 60 * 60 * .1 // expires in 24 hours
+             
+    }) 
+    res.send({   
+        token 
+    })
+})
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- nuevo/routes/users.js
+++ nuevo/routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../database/db');
 
+function getAllUsers(filtro, ordenamiento) {
+
+    return new Promise(function(resolve, reject) {
+        //The Promise constructor should catch any errors thrown on
+        //this tick. Alternately, try/catch and reject(err) on catch.
+        //alfa, edad, DESC, ASC, 
+        let query_str = "";
+        let orden = "asc"
+        if (ordenamiento != undefined && ordenamiento != "") {
+            orden = ordenamiento
+        }
+        if (filtro != undefined && filtro != "") {
+            console.log('1')
+            query_str = "SELECT * FROM USUARIO order by " + filtro + " " + orden;
+        } else {
+            query_str = "SELECT * FROM USUARIO ";
+        }
+        pool.query(query_str, function(err, rows, fields) {
+            //Call reject on error states,
+            //call resolve with results
+            if (err) {
+                return reject(err);
+            }
+            resolve(rows);
+        });
+    });
+}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    getAllUsers(req.query.filtro, req.query.orden).then(function(rows) {
+        res.status(200).jsonp(rows);
+    }).catch((err) => setImmediate(() => {
+        res.status(404).jsonp({
+            error: 'Existe un error en la consulta : (' + err + ")"
+        })
+    }));
 });
 
 module.exports = router;
