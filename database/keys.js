ff5313b9274d230d9c9f628448c90746bb0bf62a
Charly Bernabe <charly.ivan.bernabe@gmail.com>
2019-06-02 23:53:05 -0500

    Configuraciones de archivos de db

--- database/keys.js
+++ database/keys.js
@@ -4,7 +4,7 @@ module.exports = {
         connectionLimit: 10,
         host: 'https://databases-auth.000webhost.com/',
         user: 'id5737972_root1',
-        password: 'charly12#',
+        password: 'CHARLY12#',
         database: 'id5737972_usuario_test'
     }
 
