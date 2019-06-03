2ec6f730723297d5629ca177dbde4d162424539f
Charly Bernabe <charly.ivan.bernabe@gmail.com>
2019-06-03 00:04:54 -0500

    Configuraciones de bd

--- database/keys.js
+++ database/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'https://databases-auth.000webhost.com',
-        user: 'id5737972_root1',
-        password: 'CHARLY12#',
-        database: 'id5737972_usuario_test'
+        host: 'remotemysql.com',
+        user: 'h5FfgER6gz',
+        password: 'FkgAin2lr3',
+        database: 'h5FfgER6gz'
     }
 
 };
